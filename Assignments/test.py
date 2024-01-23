c0f101e3ca880b956df4dadf3cde242f88e2a33a
Ethan <134173034+Aueza@users.noreply.github.com>
2024-01-23 00:44:01 +0000

    test

--- Assignments/test.py
+++ Assignments/test.py
@@ -1,1 +1,39 @@
-print("test")
+#begin
+
+def fillList(list):
+    total = int(input("how many numbers would you like to enter: "))
+    count = 0
+    while(count < total):
+        number = getNum()
+        list.append(number)
+        print("added",number, "to the list.")
+
+        count = count+1
+    
+def getNum():
+    number = int(input("please enter a number: "))
+    return number
+
+def printList(list):
+    print("printing list...")
+    for numbers in range(len(list)):
+        print(list[numbers])
+
+def findSum(list):
+    sum = 0
+    for number in range(len(list)):
+        sum = sum + list[number]
+    return sum
+        
+
+
+
+nums = []
+fillList(nums)
+printList(nums)
+print("the sum of the list is: ", findSum(nums))
+
+
+
+
+
