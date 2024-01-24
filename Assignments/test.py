b9f1027e97f4aff8ab641e60a0970cd8c4d2cb22
Ethan <134173034+Aueza@users.noreply.github.com>
2024-01-24 22:06:02 +0000

    test

--- Assignments/test.py
+++ Assignments/test.py
@@ -1,22 +1,22 @@
-#begin
+# Ethan Saenz
+# 1-24-24
+# Bubblesorting a list with a swap function
 
 def swap(list, i, j):
-    print("moving ", list[i], "to ", list[j])
     temp = list[i]
     list[i] = list[j]
     list[j] = temp
 
 def bubblesort(list):
-    min = list[0]
-    minPos = 0
+    print("printing the sorted list, using bubblesort")
     for x in range(len(list)):
+        min = list[x]
         for y in range(len(list)):
-            if(list[y] < min):
+            if(list[y] > min):
                 min = list[y]
-                minPos = y
-                swap(list, x, minPos)  
+                swap(list, x, y)
 
-nums = [1, 56, 23, 144, 12]
+nums = [1, 56, -23, 144, -12, 15, 289]
 
 print(nums)
 bubblesort(nums)
